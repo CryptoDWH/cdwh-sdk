115c6978678f1b10b62095ea82f9fe22ce7fd7d6
wanbowen001 <wbw157@gmail.com>
2020-12-08 20:04:10 +0800

    feat: transfer sample compatibility

--- sdk-js/sample/send.money.js
+++ sdk-js/sample/send.money.js
@@ -7,12 +7,23 @@ loader.load(function(NRS) {
      *
      * loader.load()已对sdk进行初始化并载入到 NRS对象，NRS通过回调入参传入，可通过 NRS.function(params)调用sdk内部定义的方法
      */
-    var data = {
-        recipient: NRS.getAccountIdFromRS(config.recipient), // public key to account id
+
+    // 不带公钥，首次转账后账户余额不可用，需另行激活，通过该接口激活/sharder?requestType=setAccountInfo
+    var data_no_publicKey = {
+        recipient: NRS.getAccountIdFromRS(config.recipient), // accountRS to account id
         amountNQT: NRS.convertToNQT("0.23"), // MW to NQT conversion
         secretPhrase: config.secretPhrase,
         // encryptedMessageIsPrunable: "true" // Optional - make the attached message prunable
     };
+    // 默认带上公钥，转账经确认后账户自动激活
+    var data = {
+        recipient: NRS.getAccountIdFromPublicKey(config.recipientPublicKey), // public key to account id
+        amountNQT: NRS.convertToNQT("0.23"), // MW to NQT conversion
+        recipientPublicKey: config.recipientPublicKey, // Optional - public key announcement to init a new account
+        secretPhrase: config.secretPhrase,
+        // encryptedMessageIsPrunable: "true" // Optional - make the attached message prunable
+    };
+
     // Compose the request data
     data = Object.assign(
         data,
